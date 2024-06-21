a05438e8715e0dda3fd257fc41fcc1b124ff420d
DiegoFariasDF <86974387+DiegoFariasDF@users.noreply.github.com>
2024-06-21 08:55:12 -0300

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-/*import './App.css';*/
 import Header from './Header';
 import Backtop from './Backtop';
 import Sobre from './Sobre';
 import Projetos from './Projetos';
 import Form from './Form';
 import Rodape from './Rodape';
+
 
 
 function App() {
